22b36da24ff42b0b7d082dae0b695589b4ce54bc
Nick Duckwiler <nduckwiler@gmail.com>
2020-11-09 19:56:34 -0500

    Add transpilation script and rewire

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import dependency from './dependency';
+
+const newDependency = dependency;
+
+export const Dog = {
+  name: "Fido",
+  bark: function() { return "arf!"},
+  useDependency: function() { return newDependency(); }
+}
+
+const useless = Dog.bark();
+const useless2 = Dog.useDependency();
+
+console.log('index.js executed successfully');
--- test/test.js
+++ test/test.js
@@ -1,8 +1,50 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.strictEqual([1, 2, 3].indexOf(4), -1);
+var rewire = require('rewire');
+
+import { Dog } from '../lib/index';
+
+describe('Dog', function() {
+  describe('name', function() {
+    it('should be Fido', function() {
+      assert.strictEqual(Dog.name, "Fido");
     });
+  });
+});
+
+describe('rewire', function() {
+  describe('__get__', function() {
+    it('should work with ES6 modules', function() {
+      const appModule = rewire('../lib/index');
+      const rewiredDog = appModule.__get__("Dog");
+
+      assert.strictEqual(rewiredDog.name, "Fido");
+    })
+  });
+
+  const mock = function() {
+    return 'ima mock'
+  };
+
+  describe('__set__', function() {
+    it('should work with ES6 modules', function() {
+      const appModule = rewire('../lib/index');
+      appModule.__set__("dependency", mock);
+      
+      assert.strictEqual(appModule.Dog.useDependency(), "ima mock");
+    });
+
+    it('should work with ES6 modules when we rename them', function() {
+      const appModule = rewire('../lib/index');
+      appModule.__set__("newDependency", mock);
+
+      assert.strictEqual(appModule.Dog.useDependency(), "ima mock");
+    });
+
+    it('should work with ES6 modules if we use transpiled names', function() {
+      const appModule = rewire('../lib/index');
+      appModule.__set__("_dependency", mock);
+
+      assert.strictEqual(appModule.Dog.useDependency(), "ima mock");
+    })
   });
 });
